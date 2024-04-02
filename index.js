234a53ce4ce92abdc3d82e7a63f79e9a94e714b6
Ismat Fatima <159720166+Fatima-Ismat@users.noreply.github.com>
2024-04-03 02:30:15 +0500

    number guessing

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt({
+    name: "randomNumber",
+    type: "number",
+    message: "guess the number"
+});
+if (answer.randomNumber === randomNumber) {
+    console.log("congratulations your guessing number is correct");
+}
+else {
+    console.log("Your guessing number is wrong please try again!");
+}
